81763d95c43e4257faebf9caeecd151878fc48e5
maria_aristizabal <mariaimcs@gmail.com>
2021-07-19 16:50:11 -0400

    Fix bug in the function Geo_coord_to_GOFS_coord and updated print out message

--- Working_examples.py
+++ Working_examples.py
@@ -6,7 +6,7 @@ def Geo_coord_to_GOFS_coord(long,latg):
     if np.ndim(long) > 0:
         lonm = [360 + ln if ln<0 else ln for ln in long]
     else:
-        lonm = [360 + long if long<0 else ii][0]
+        lonm = [360 + long if long<0 else long][0]
     latm = latg
 
     return lonm, latm
@@ -61,8 +61,8 @@ dens_model = sw.dens(salt_model,temp_model,depth_model)
 # at (longitude,latitude) = (-70,24) on July 3 2020 at 00 UTC
 
 OHC = OHC_from_profile(depth_model,temp_model,dens_model)
-print('The OHC from GOFS3.1 at (lon,lat) = (-71,24) on July 3 2020 at 00 UTC is ',\
-      OHC, ' kJ/m^2 \n')
+print('The OHC from GOFS3.1 at (lon,lat) = (' + str(long)+','+str(latg)+') on '+ date_ini +' is ',\
+      + OHC, ' kJ/m^2 \n')
 
 ################################################################################
 #%% Example2: Mixed layer depth and Mixed layer temperature based on the
@@ -77,8 +77,8 @@ dtemp = 0.2
 ref_depth = 10 # meters
 MLD_temp_crit, MLT_temp_crit = MLD_temp_crit(dtemp,ref_depth,depth_model,\
                                              temp_model)
-print('The mixed layer temperature (based on a temperatere criteria) from GOFS3.1 at (lon,lat) = (-71,24) on July 3 2020 at 00 UTC is ',\
-      MLT_temp_crit, ' degrees C \n')
+print('The mixed layer temperature (based on a temperatere criteria) from GOFS3.1 at (lon,lat) =  ('\
+      + str(long)+','+str(latg)+') on '+ date_ini +' is ', MLT_temp_crit, ' degrees C \n')
 
 ################################################################################
 #%% Example3: Mixed layer depth and Mixed layer temperature based on the
@@ -93,8 +93,8 @@ drho = 0.125
 ref_depth = 10 # meters
 MLD_dens_crit, MLT_dens_crit = MLD_dens_crit(drho,ref_depth,depth_model,\
                                              temp_model,dens_model)
-print('The mixed layer temperature (based on a density criteria) from GOFS3.1 at (lon,lat) = (-71,24) on July 3 2020 at 00 UTC is ',\
-      MLT_dens_crit, ' degrees C \n')
+print('The mixed layer temperature (based on a density criteria) from GOFS3.1 at (lon,lat) =  ('\
+      + str(long)+','+str(latg)+') on '+ date_ini +' is ', MLT_dens_crit, ' degrees C \n')
 
 ################################################################################
 #%% Example4: Depth average temperature in the top 100 meters
@@ -102,15 +102,15 @@ print('The mixed layer temperature (based on a density criteria) from GOFS3.1 at
 # at (longitude,latitude) = (-70,24) on July 3 2020 at 00 UTC
 
 temp100 = T100(depth_model,temp_model)
-print('The depth average temperature in the top 100 meters from GOFS3.1 at (lon,lat) = (-71,24) on July 3 2020 at 00 UTC is ',\
-      temp100, ' degrees C \n')
+print('The depth average temperature in the top 100 meters from GOFS3.1 at (lon,lat) =  ('\
+      + str(long)+','+str(latg)+') on '+ date_ini +' is ', temp100, ' degrees C \n')
 ################################################################################
 #%% Example5: Potential energy anomaly (J/m^3) in the top 100 meters of the water column
 # using the global ocean model GOFS3.1
 # at (longitude,latitude) = (-70,24) on July 3 2020 at 00 UTC
 
 PEA = Potential_energy_anomaly100(depth_model,dens_model)
-print('The potential energy anomaly in the top 100 meters from GOFS3.1 at (lon,lat) = (-71,24) on July 3 2020 at 00 UTC is ',\
-      PEA, ' J/m^3 \n')
+print('The potential energy anomaly in the top 100 meters from GOFS3.1 at (lon,lat) =  ('\
+      + str(long)+','+str(latg)+') on '+ date_ini +' is ', PEA, ' J/m^3 \n')
 
 ################################################################################
